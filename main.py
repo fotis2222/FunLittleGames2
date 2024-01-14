7ef0b6e65cc87d736933c89d3ff39c5cb94aa713
fotis222 <nitsas2012@gmail.com>
2024-01-14 16:07:25 +0200

    added guess the number (v2 in the original)

--- main.py
+++ main.py
@@ -14,6 +14,19 @@ select a game:
 (1) guess the number (hints)""")
 game: str = input()
 if game == "1":
-    pass
+    secretNumber = randint(1, 10)
+    guess: int
+    numOfGuesses = 0
+    print("try to guess the secret number in 3 guesses!")
+    while numOfGuesses < 3:
+        print("Guess: ")
+        guess = int(input())
+        if guess == secretNumber:
+            print("you win!")
+            enter()
+        numOfGuesses += 1
+    else:
+        print("you lose")
+        enter()
 else:
     explain()
